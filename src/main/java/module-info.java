99876843a304c37edcfb4e3c657d66603f1f7b20
Fernando Bruno <fbrunofa8@gmail.com>
2025-10-18 19:52:20 -0300

    Correção do erro do App

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,8 @@
 module com.trabrobotartaruga.robo_tartaruga {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires transitive javafx.graphics;
+    
     opens com.trabrobotartaruga.robo_tartaruga to javafx.fxml;
     exports com.trabrobotartaruga.robo_tartaruga;
 }
